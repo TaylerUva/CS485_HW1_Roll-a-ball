ba55fafc5bc74ce2821dce2fcc1d12f3c5ad9a9e
Tayler Uva <tayleruva@gmail.com>
2019-02-03 23:32:07 -0800

    added jump and out of bounds

--- Assets/Gameplay/PlayerController.cs
+++ Assets/Gameplay/PlayerController.cs
@@ -4,19 +4,27 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
-	public float speed;
+	public float speed = 10;
+	public float jumpHeight = 1;
+
 	public Text countText;
 	public Text winText;
+	public Text loseText;
 
 	private Rigidbody rb;
-	private int count;
+	private int score;
+	private bool isGrounded;
 
 	void Start() {
 		rb = GetComponent<Rigidbody>();
-		count = 0;
+		score = 0;
 		SetCountText();
 		winText.gameObject.SetActive(false);
-		winText.text = "";
+		loseText.gameObject.SetActive(false);
+	}
+
+	private void LateUpdate() {
+		checkOutOfBounds();
 	}
 
 	void FixedUpdate() {
@@ -25,22 +33,39 @@ public class PlayerController : MonoBehaviour {
 
 		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
+		if (Input.GetButtonDown("Jump") && isGrounded) {
+			rb.AddForce(Vector3.up * Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y), ForceMode.VelocityChange);
+			isGrounded = false;
+		}
+
 		rb.AddForce(movement * speed);
 	}
 
 	void OnTriggerEnter(Collider other) {
 		if (other.gameObject.CompareTag("Pick Up")) {
 			other.gameObject.SetActive(false);
-			count = count + 1;
+			score = score + 1;
 			SetCountText();
 		}
 	}
 
 	void SetCountText() {
-		countText.text = "Count: " + count.ToString();
-		if (count >= 8) {
+		countText.text = "Score: " + score.ToString();
+		if (score >= 8) {
 			winText.gameObject.SetActive(true);
-			winText.text = "You Win!";
 		}
 	}
+
+	void OnCollisionEnter(Collision other) {
+		if (other.gameObject.CompareTag("Ground")) {
+			isGrounded = true;
+		}
+	}
+
+	void checkOutOfBounds() {
+		if (gameObject.transform.position.y < -5) {
+			loseText.gameObject.SetActive(true);
+		}
+	}
+
 }
--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-# HW1_Roll-a-ball
+# HW Assignment 1: Roll-a-ball
+
+Create by Tayler Uva  
+Project [Link](https://github.com/TaylerUva/CS485_HW1_Roll-a-ball) [1]
 
 Based on the Unity Tutorial: [Roll-a-ball](https://unity3d.com/learn/tutorials/s/roll-ball-tutorial)
-
 
 ## Changes from tutorial
 
@@ -9,22 +11,32 @@ Based on the Unity Tutorial: [Roll-a-ball](https://unity3d.com/learn/tutorials/s
 - File structure
 - Reduced cube numbers
 - Added restart buttons
+- Added game over and out of bounds
 
 ---
 
 ## External References
 
 LoadSceneOnClick.cs used from previous project:  
-[SlugShuttle](https://github.com/TaylerUva/SlugShuttle)
+[SlugShuttle](https://github.com/TaylerUva/SlugShuttle) [2]
 
-Jump code used from instructor provided SimpleProject2:  
-[SimpleProject2 RigiController](hhttps://github.com/xye-csusm/SimpleProject2/blob/master/Assets/RigiController.cs)
+Some jump code used from instructor provided SimpleProject2:  
+[SimpleProject2 RigiController](hhttps://github.com/xye-csusm/SimpleProject2/blob/master/Assets/RigiController.cs) [3]
 
 ---
 
 ## Furture improvements to implement
 
-- [ ] Add jump
+- [x] Add jump
 - [ ] Add goal
 - [ ] Add levels
 - [ ] Add time for score
+- [ ] Add menu
+
+---
+
+### Raw URLs
+
+1. https://github.com/TaylerUva/CS485_HW1_Roll-a-ball
+2. https://github.com/TaylerUva/SlugShuttle
+3. https://github.com/xye-csusm/SimpleProject2/blob/master/Assets/RigiController.cs
